d5bf0490f28a0dcca4a9450b6838f1ba8f180e0d
yangjun <345445095@qq.com>
2024-09-17 19:51:02 +0800

    传入

--- application.9beb6.js
+++ application.9beb6.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.5423a.json';
+          this.settingsPath = 'src/settings.1d70d.json';
           this.showFPS = true;
         }
         _createClass(Application, [{
--- index.f3b49.js
+++ index.f3b49.js
@@ -1,4 +1,4 @@
-System.register(["./application.a88cd.js"], function (_export, _context) {
+System.register(["./application.9beb6.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
